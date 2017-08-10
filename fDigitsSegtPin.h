7f001d9e822278173e301f10ed8d720009a5eb7d
KLsz <klsz@outlook.com>
2017-08-10 10:19:05 +0800

    fDigitsSegtPin.h: finish the pin controls;

--- fDigitsSegtPin.h
+++ fDigitsSegtPin.h
@@ -2,10 +2,43 @@ class fDigitsSegtPin {
     public:
         //Storage and init the pins
         fDigitsSegtPin(u8 vPf1, u8 vPf2, u8 vPf3, u8 vPf4, u8 vPf5, u8 vPf6, u8 vPf7, u8 vPf8, u8 vPf9, u8 vPf10, u8 vPf11, u8 vPf12);
+
+        //Print digit
+        void print(float vffInput);
     private:
         //Pins
         u8 vPcD1; u8 vPcD2; u8 vPcD3; vPcD4;
         u8 vPcA; u8 vPcB; u8 vPcC; u8 vPcD; u8 vPcE; u8 vPcF; u8 vPcG; u8 vPcH;
+
+        //Table
+        u8 villDigitTable[10][7] = {
+        //   a  b  c  d  e  f  g   
+            {1, 1, 1, 1, 1, 1, 0}, //0
+            {0, 1, 1, 0, 0, 0, 0}, //1
+            {1, 1, 0, 1, 1, 0, 1}, //2
+            {1, 1, 1, 1, 0, 0, 1}, //3
+            {0, 1, 1, 0, 0, 1, 1}, //4
+            {1, 0, 1, 1, 0, 1, 1}, //5
+            {1, 0, 1, 1, 1, 1, 1}, //6
+            {1, 1, 1, 0, 0, 0, 0}, //7
+            {1, 1, 1, 1, 1, 1, 1}, //8
+            {1, 1, 1, 1, 0, 1, 1}, //9
+        };
+
+        //Clean the afterglow
+        void fvAfterGlow();
+        //Set the digit to write
+        void fvSet(u8 vifDigit);
+        //Write number to the digit
+        void fvWrite(u8 vifNumber);
+
+        //Clear the digit
+        void fvClear(u8 vifDigit);
+        //Print the digit with the number
+        void fvPrint(u8 vifDigit, u8 vifNumber);
+
+        //Rounding function used in printing
+        u8 fiRound(float vff);
 }
 
 //Storage and init the pins
@@ -19,74 +52,63 @@ fDigitsSegtPin::fDigitsSegtPin(u8 vPf1, u8 vPf2, u8 vPf3, u8 vPf4, u8 vPf5, u8 v
     digitalWrite(vPcA, LOW); digitalWrite(vPcB, LOW); digitalWrite(vPcC, LOW); digitalWrite(vPcD, LOW); digitalWrite(vPcE, LOW); digitalWrite(vPcF, LOW); digitalWrite(vPcG, LOW); digitalWrite(vPcH, LOW);
 }
 
+//Clean the afterglow
+void fDigitsSegtPin::fvAfterGlow() {
+    digitalWrite(vPcA,LOW);
+    digitalWrite(vPcB,LOW);
+    digitalWrite(vPcC,LOW);
+    digitalWrite(vPcD,LOW);
+    digitalWrite(vPcE,LOW);
+    digitalWrite(vPcF,LOW);
+    digitalWrite(vPcG,LOW);
+    digitalWrite(vPcH,LOW);
+}
+//Set the digit to write
+void fDigitsSegtPin::fvSet(u8 vifDigit) {
+    switch(vifDigit) {
+        case 1: digitalWrite(vPcD1, LOW); digitalWrite(vPcD2, HIGH); digitalWrite(vPcD3, HIGH); digitalWrite(vPcD4, HIGH); break;
+        case 2: digitalWrite(vPcD1, HIGH); digitalWrite(vPcD2, LOW); digitalWrite(vPcD3, HIGH); digitalWrite(vPcD4, HIGH); break;
+        case 3: digitalWrite(vPcD1, HIGH); digitalWrite(vPcD2, HIGH); digitalWrite(vPcD3, LOW); digitalWrite(vPcD4, HIGH); break;
+        case 4: digitalWrite(vPcD1, HIGH); digitalWrite(vPcD2, HIGH); digitalWrite(vPcD3, HIGH); digitalWrite(vPcD4, LOW); break;
+        default: break;
+    }
+}
+//Write number to the digit
+void fDigitsSegtPin::fvWrite(u8 vifNumber) {
+    digitalWrite(vPcA, villDigitTable[vifNumber][0]);
+    digitalWrite(vPcB, villDigitTable[vifNumber][1]);
+    digitalWrite(vPcC, villDigitTable[vifNumber][2]);
+    digitalWrite(vPcD, villDigitTable[vifNumber][3]);
+    digitalWrite(vPcE, villDigitTable[vifNumber][4]);
+    digitalWrite(vPcF, villDigitTable[vifNumber][5]);
+    digitalWrite(vPcG, villDigitTable[vifNumber][6]);
+}
+
+//Clear the digit
+void fDigitsSegtPin::fvClear(u8 vifDigit) {
+    fvAfterGlow();
+    fvSet(vifDigit);
+//    delay(2); ----------------------------------------------------!
+}
+//Print the digit with the number
+void fDigitsSegtPin::fvPrint(u8 vifDigit, u8 vifNumber) {
+    fvAfterGlow();
+    fvSet(vifDigit);
+    fvWrite(vifNumber);
+//    delay(2); ----------------------------------------------------!
+}
+
+u8 fiRound(float vff) { return((vff - (int)(vff) >= 0.5) ? ((int)(vff) + 1) : ((int)(vff))); }
+void fDigitsSegtPin::print(float vffInput) {
+}
+
 /*-------------------------------------------------------------------
  *                  Origin code from STF_A_3                         
  *-------------------------------------------------------------------
  */
 
-//[INNER]Storge the 4-digit display table
-u8 villDigitTable[11][7] = {
-//   a  b  c  d  e  f  g  h     
-    {1, 1, 1, 1, 1, 1, 0}, //0
-    {0, 1, 1, 0, 0, 0, 0}, //1
-    {1, 1, 0, 1, 1, 0, 1}, //2
-    {1, 1, 1, 1, 0, 0, 1}, //3
-    {0, 1, 1, 0, 0, 1, 1}, //4
-    {1, 0, 1, 1, 0, 1, 1}, //5
-    {1, 0, 1, 1, 1, 1, 1}, //6
-    {1, 1, 1, 0, 0, 0, 0}, //7
-    {1, 1, 1, 1, 1, 1, 1}, //8
-    {1, 1, 1, 1, 0, 1, 1}, //9
-};
-//[INNER]Clean the afterglow using fvAfterGlow();
-#define fvAfterGlow() { \
-    digitalWrite(vPcA,LOW); \
-    digitalWrite(vPcB,LOW); \
-    digitalWrite(vPcC,LOW); \
-    digitalWrite(vPcD,LOW); \
-    digitalWrite(vPcE,LOW); \
-    digitalWrite(vPcF,LOW); \
-    digitalWrite(vPcG,LOW); \
-    digitalWrite(vPcH,LOW); \
-}
-//[INNER]Set the digit to write using fvSet(viDigit);
-#define fvSet(vifDigit) { \
-    switch(vifDigit) { \
-        case 1: digitalWrite(vPcD1, LOW); digitalWrite(vPcD2, HIGH); digitalWrite(vPcD3, HIGH); digitalWrite(vPcD4, HIGH); break; \
-        case 2: digitalWrite(vPcD1, HIGH); digitalWrite(vPcD2, LOW); digitalWrite(vPcD3, HIGH); digitalWrite(vPcD4, HIGH); break; \
-        case 3: digitalWrite(vPcD1, HIGH); digitalWrite(vPcD2, HIGH); digitalWrite(vPcD3, LOW); digitalWrite(vPcD4, HIGH); break; \
-        case 4: digitalWrite(vPcD1, HIGH); digitalWrite(vPcD2, HIGH); digitalWrite(vPcD3, HIGH); digitalWrite(vPcD4, LOW); break; \
-        default: break; \
-    } \
-}
-//[INNER]Write the number using fvWrite(viNumber, vifDot);
-#define fvWrite(vifNumber, vifDot) { \
-    digitalWrite(vPcA, villDigitTable[vifNumber][0]); \
-    digitalWrite(vPcB, villDigitTable[vifNumber][1]); \
-    digitalWrite(vPcC, villDigitTable[vifNumber][2]); \
-    digitalWrite(vPcD, villDigitTable[vifNumber][3]); \
-    digitalWrite(vPcE, villDigitTable[vifNumber][4]); \
-    digitalWrite(vPcF, villDigitTable[vifNumber][5]); \
-    digitalWrite(vPcG, villDigitTable[vifNumber][6]); \
-    digitalWrite(vPcH, vifDot); \
-}
-//[INNER]Clear the digit using fvClear(viDigit)
-#define fvClear(vifDigit) { \
-    fvAfterGlow(); \
-    fvSet(vifDigit); \
-    delay(2); \
-}
-//[INNER]Print the digit with the number using fvPrint(viDigit, viNumber, viDot);
-#define fvPrint(vifDigit, vifNumber, vifDot) { \
-    fvAfterGlow(); \
-    fvSet(vifDigit); \
-    fvWrite(vifNumber, vifDot); \
-    delay(2); \
-}
-//[INNER]Round the float to int using fiRound(vfFloat);
-u8 fiRound(float vff) { return((vff - (int)(vff) >= 0.5) ? ((int)(vff) + 1) : ((int)(vff))); }
 //[OUTER]Print the Recorded Voltage through the 4-digit display using fv();
-#define fv() { \
+#define fvDigit() { \
     if(vfRead <= 5) { \
         fvPrint(1, fiRound(vfRead), 1); \
         vfRead -= fiRound(vfRead); \
