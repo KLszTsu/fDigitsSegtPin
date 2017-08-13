d7ae4481ab29fe6360edb7dd3bd854f0c225cab5
KLsz <klsz@outlook.com>
2017-08-13 21:09:28 +0800

    fDigitsSegtPin.h: finish the print part, add a parameter of doPrint_lastZero, ready for testing;

--- fDigitsSegtPin.h
+++ fDigitsSegtPin.h
@@ -5,12 +5,15 @@ class fDigitsSegtPin {
 
         //Print digit
         void print(float vffInput);
+
+        //Settings
+        u8 doPrint_lastDot = 0;
     private:
         //Pins
         u8 vPcD1; u8 vPcD2; u8 vPcD3; vPcD4;
         u8 vPcA; u8 vPcB; u8 vPcC; u8 vPcD; u8 vPcE; u8 vPcF; u8 vPcG; u8 vPcH;
 
-        //Table
+        //Display table
         u8 villDigitTable[10][7] = {
         //   a  b  c  d  e  f  g   
             {1, 1, 1, 1, 1, 1, 0}, //0
@@ -54,14 +57,8 @@ fDigitsSegtPin::fDigitsSegtPin(u8 vPf1, u8 vPf2, u8 vPf3, u8 vPf4, u8 vPf5, u8 v
 
 //Clean the afterglow
 void fDigitsSegtPin::fvAfterGlow() {
-    digitalWrite(vPcA,LOW);
-    digitalWrite(vPcB,LOW);
-    digitalWrite(vPcC,LOW);
-    digitalWrite(vPcD,LOW);
-    digitalWrite(vPcE,LOW);
-    digitalWrite(vPcF,LOW);
-    digitalWrite(vPcG,LOW);
-    digitalWrite(vPcH,LOW);
+    digitalWrite(vPcA, LOW); digitalWrite(vPcB, LOW); digitalWrite(vPcC, LOW); digitalWrite(vPcD, LOW); digitalWrite(vPcE, LOW); digitalWrite(vPcF, LOW); digitalWrite(vPcG, LOW);
+    digitalWrite(vPcH, LOW);
 }
 //Set the digit to write
 void fDigitsSegtPin::fvSet(u8 vifDigit) {
@@ -75,13 +72,7 @@ void fDigitsSegtPin::fvSet(u8 vifDigit) {
 }
 //Write number to the digit
 void fDigitsSegtPin::fvWrite(u8 vifNumber) {
-    digitalWrite(vPcA, villDigitTable[vifNumber][0]);
-    digitalWrite(vPcB, villDigitTable[vifNumber][1]);
-    digitalWrite(vPcC, villDigitTable[vifNumber][2]);
-    digitalWrite(vPcD, villDigitTable[vifNumber][3]);
-    digitalWrite(vPcE, villDigitTable[vifNumber][4]);
-    digitalWrite(vPcF, villDigitTable[vifNumber][5]);
-    digitalWrite(vPcG, villDigitTable[vifNumber][6]);
+    digitalWrite(vPcA, villDigitTable[vifNumber][0]); digitalWrite(vPcB, villDigitTable[vifNumber][1]); digitalWrite(vPcC, villDigitTable[vifNumber][2]); digitalWrite(vPcD, villDigitTable[vifNumber][3]); digitalWrite(vPcE, villDigitTable[vifNumber][4]); digitalWrite(vPcF, villDigitTable[vifNumber][5]); digitalWrite(vPcG, villDigitTable[vifNumber][6]);
 }
 
 //Clear the digit
@@ -90,10 +81,15 @@ void fDigitsSegtPin::fvClear(u8 vifDigit) {
     fvSet(vifDigit);
 //    delay(2); ----------------------------------------------------!
 }
-//Print the digit with the number
-void fDigitsSegtPin::fvPrint(u8 vifDigit, u8 vifNumber) {
+//Print the dot
+void fDigitsSegtPin::fvDot(u8 vifDigit) {
     fvAfterGlow();
     fvSet(vifDigit);
+    digitalWrite(vPcH, HIGH);
+//    delay(2); ----------------------------------------------------!
+}
+//Print the digit with the number
+void fDigitsSegtPin::fvPrint(u8 vifDigit, u8 vifNumber) {
     fvWrite(vifNumber);
 //    delay(2); ----------------------------------------------------!
 }
@@ -102,37 +98,42 @@ u8 fiRound(float vff) { return((vff - (int)(vff) >= 0.5) ? ((int)(vff) + 1) : ((
 void fDigitsSegtPin::print(float vffInput) {
 }
 
-/*-------------------------------------------------------------------
- *                  Origin code from STF_A_3                         
- *-------------------------------------------------------------------
- */
+//Print user's input
+void fDigitsSegtPin::print(float vffInput) {
+    //Make sure these value can be printed
+    if(vffInput >= 10000) {
+        if(Serial) Serial.println("Error(4Digit7Seg12Pin): Input value larger than 10000.");
+        break;
+    }
+    if(vffInput < 0) {
+        if(Serial) Serial.println("Error(4Digit7Seg12Pin): Input value smaller than 0.");
+        break;
+    }
 
-//[OUTER]Print the Recorded Voltage through the 4-digit display using fv();
-#define fvDigit() { \
-    if(vfRead <= 5) { \
-        fvPrint(1, fiRound(vfRead), 1); \
-        vfRead -= fiRound(vfRead); \
-        vfRead *= 10; \
-        fvPrint(2, fiRound(vfRead), 0); \
-        vfRead -= fiRound(vfRead); \
-        vfRead *= 10; \
-        fvPrint(3, fiRound(vfRead), 0); \
-        vfRead -= fiRound(vfRead); \
-        vfRead *= 10; \
-        fvPrint(4, fiRound(vfRead), 0); \
-    } \
-    else { \
-        vfRead /= 10; \
-        fvPrint(1, fiRound(vfRead), 0); \
-        vfRead -= fiRound(vfRead); \
-        vfRead *= 10; \
-        fvPrint(2, fiRound(vfRead), 1); \
-        vfRead -= fiRound(vfRead); \
-        vfRead *= 10; \
-        fvPrint(3, fiRound(vfRead), 0); \
-        vfRead -= fiRound(vfRead); \
-        vfRead *= 10; \
-        fvPrint(4, fiRound(vfRead), 0); \
-    } \
+    //Print the dot and precondition the value
+    if(vffInput >= 1000) {
+        vff /= 1000;
+        if(doPrint_lastDot == 1) fvDot(4);
+    }
+    else if(vffInput >= 100) {
+        vff /= 100;
+        fvDot(3);
+    }
+    else if(vffInput >= 10) {
+        vff /= 10;
+        fvDot(2);
+    }
+    else if(vffInput >= 1) {
+        fvDot(1);
+    }
+
+    //Print the value
+    fvPrint(1, fiRound(vff));
+    vff -= fiRound(vff); vff *= 10;
+    fvPrint(2, fiRound(vff));
+    vff -= fiRound(vff); vff *= 10;
+    fvPrint(3, fiRound(vff));
+    vff -= fiRound(vff); vff *= 10;
+    fvPrint(4, fiRound(vff));
 }
 
